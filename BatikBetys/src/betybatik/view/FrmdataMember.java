4c1bc7e8c9e63e455d7417bd9eb6f6dd0312a686
ardyansyahr <rahasia.saya15@gmail.com>
2018-12-18 12:16:01 +0700

    form update

--- BatikBetys/src/betybatik/view/FrmdataMember.java
+++ BatikBetys/src/betybatik/view/FrmdataMember.java
@@ -9,12 +9,12 @@ package betybatik.view;
  *
  * @author ASUS
  */
-public class FrmMember extends javax.swing.JFrame {
+public class FrmdataMember extends javax.swing.JFrame {
 
     /**
      * Creates new form FrmMember
      */
-    public FrmMember() {
+    public FrmdataMember() {
         initComponents();
     }
 
@@ -166,20 +166,21 @@ public class FrmMember extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmMember().setVisible(true);
+                new FrmdataMember().setVisible(true);
             }
         });
     }
--- BatikBetys/src/betybatik/view/FrmdataSupplier.java
+++ BatikBetys/src/betybatik/view/FrmdataSupplier.java
@@ -9,12 +9,12 @@ package betybatik.view;
  *
  * @author ASUS
  */
-public class FrmSupplier extends javax.swing.JFrame {
+public class FrmdataSupplier extends javax.swing.JFrame {
 
     /**
      * Creates new form FrmSupplier
      */
-    public FrmSupplier() {
+    public FrmdataSupplier() {
         initComponents();
     }
 
@@ -214,20 +214,21 @@ public class FrmSupplier extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmdataSupplier.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmSupplier().setVisible(true);
+                new FrmdataSupplier().setVisible(true);
             }
         });
     }
