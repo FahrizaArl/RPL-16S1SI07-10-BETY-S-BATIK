0b2d81908449a2917f32fc61144703b51d66207e
ardyansyahr <rahasia.saya15@gmail.com>
2019-01-14 02:58:42 +0700

    New update

---
+++ SourceCode/BatikBetys/src/betybatik/model/supplier.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package betybatik.model;
+
+/**
+ *
+ * @author ASRock
+ */
+public class supplier {
+    
+   private String IdSupplier;
+    private String NamaSupplier;
+    private String Alamat;
+    private String NoTelp;
+
+    public String getIdSupplier() {
+        return IdSupplier;
+    }
+
+    public void setIdSupplier(String IdSupplier) {
+        this.IdSupplier = IdSupplier;
+    }
+
+    public String getNamaSupplier() {
+        return NamaSupplier;
+    }
+
+    public void setNamaSupplier(String NamaSupplier) {
+        this.NamaSupplier = NamaSupplier;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+
+    public String getNoTelp() {
+        return NoTelp;
+    }
+
+    public void setNoTelp(String NoTelp) {
+        this.NoTelp = NoTelp;
+    }
+
+}
